dfab9c0c14a72ffa47b975ea2bbdd2401ec35d5a
Blurring Shadow <976999484@qq.com>
2021-08-09 13:57:03 +0800

    feat(property): object
    add fixed object, the object is only referable

--- include/utility/traits/object.h
+++ include/utility/traits/object.h
@@ -9,11 +9,21 @@ namespace blurringshadow::utility::traits
     struct unique_object
     {
         unique_object() noexcept = default;
-        unique_object(const unique_object&) = delete;
+        unique_object(const unique_object&) noexcept = delete;
         unique_object(unique_object&&) noexcept = default;
-        unique_object& operator=(const unique_object&) = delete;
+        unique_object& operator=(const unique_object&) noexcept = delete;
         unique_object& operator=(unique_object&&) noexcept = default;
         ~unique_object() = default;
+    };
+
+    struct fixed_object
+    {
+        fixed_object() noexcept = default;
+        fixed_object(const fixed_object&) noexcept = delete;
+        fixed_object(fixed_object&&) noexcept = delete;
+        fixed_object& operator=(const fixed_object&) noexcept = delete;
+        fixed_object& operator=(fixed_object&&) noexcept = delete;
+        ~fixed_object() = default;
     };
 
     template<typename T>
