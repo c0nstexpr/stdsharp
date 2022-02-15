6785e77a49746a463d8be772410565cda3ab6c1d
残象之形 <976999484@qq.com>
2022-02-15 11:19:49 +0800

    feat(functional-projected-invoke): make projected_invoke as cpo

--- include/functional/projection.h
+++ include/functional/projection.h
@@ -1,29 +1,58 @@
 #pragma once
 #include "utility/value_wrapper.h"
 #include "functional/invocables.h"
+#include "functional/cpo.h"
 
 namespace stdsharp::functional
 {
-    inline constexpr auto projected_invoke = []<typename Fn, typename Projector, typename... Args>
-        requires(::std::invocable<Projector&, Args>&&...)
-    &&::std::invocable<Fn, ::std::invoke_result_t<Projector, Args>...>(
-          Fn&& fn,
-          Projector projector,
-          Args&&... args // clang-format off
-    ) noexcept(concepts::nothrow_invocable<Fn, ::std::invoke_result_t<Projector, Args>...>) // clang-format on
-          ->decltype(auto)
+    inline constexpr struct projected_invoke_fn
     {
-        return ::std::invoke(
-            ::std::forward<Fn>(fn), //
-            ::std::invoke(projector, ::std::forward<Args>(args))... //
-        );
-    };
+    private:
+        struct impl
+        {
+            template<typename Fn, typename Projector, typename... Args>
+                requires(
+                    (::std::invocable<Projector&, Args> && ...) &&
+                    ::std::invocable<Fn, ::std::invoke_result_t<Projector, Args>...> //
+                )
+            constexpr decltype(auto) operator()(Fn&& fn, Projector projector, Args&&... args) const
+                noexcept(
+                    concepts::nothrow_invocable<Fn, ::std::invoke_result_t<Projector, Args>...> //
+                )
 
-    template<typename Fn, typename Projector, typename... Args>
-    concept projected_invocable = ::std::invocable<Fn, Projector, Args...>;
+            {
+                return ::std::invoke(
+                    ::std::forward<Fn>(fn),
+                    ::std::invoke(projector, ::std::forward<Args>(args))... //
+                );
+            }
+        };
 
-    template<typename Fn, typename Projector, typename... Args>
-    concept projected_nothrow_invocable = concepts::nothrow_invocable<Fn, Projector, Args...>;
+    public:
+        template<typename... Args>
+            requires(
+                !functional::cpo_invocable<projected_invoke_fn, Args...> &&
+                ::std::invocable<impl, Args...> //
+            )
+        constexpr decltype(auto) operator()(Args&&... args) const //
+        {
+            return impl{}(::std::forward<Args>(args)...);
+        }
+
+        template<typename... Args>
+            requires functional::cpo_invocable<projected_invoke_fn, Args...>
+        constexpr decltype(auto) operator()(Args&&... args) const
+        {
+            return functional::cpo(*this, ::std::forward<Args>(args)...);
+        }
+
+    } projected_invoke{};
+
+    template<typename... Args>
+    concept projected_invocable = ::std::invocable<projected_invoke_fn, Args...>;
+
+    template<typename... Args>
+    concept projected_nothrow_invocable = concepts::nothrow_invocable<projected_invoke_fn, Args...>;
 
     template<typename Proj>
     struct projector : value_wrapper<Proj>
@@ -59,4 +88,6 @@ namespace stdsharp::functional
             return projector{::std::forward<Func>(func)}; //
         } //
     );
+
+    using make_projector_fn = decltype(make_projector);
 }
