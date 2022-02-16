1e01d29de2e73d44206cd3d9739c97fc92df9a8d
残象之形 <976999484@qq.com>
2022-02-16 09:56:53 +0800

    refactor(functional-projection): undo make projected_invoke as cpo

--- include/functional/projection.h
+++ include/functional/projection.h
@@ -7,45 +7,21 @@ namespace stdsharp::functional
 {
     inline constexpr struct projected_invoke_fn
     {
-    private:
-        struct impl
-        {
-            template<typename Fn, typename Projector, typename... Args>
-                requires(
-                    (::std::invocable<Projector&, Args> && ...) &&
-                    ::std::invocable<Fn, ::std::invoke_result_t<Projector, Args>...> //
-                )
-            constexpr decltype(auto) operator()(Fn&& fn, Projector projector, Args&&... args) const
-                noexcept(
-                    concepts::nothrow_invocable<Fn, ::std::invoke_result_t<Projector, Args>...> //
-                )
-
-            {
-                return ::std::invoke(
-                    ::std::forward<Fn>(fn),
-                    ::std::invoke(projector, ::std::forward<Args>(args))... //
-                );
-            }
-        };
-
-    public:
-        template<typename... Args>
+        template<typename Fn, typename Projector, typename... Args>
             requires(
-                !functional::cpo_invocable<projected_invoke_fn, Args...> &&
-                ::std::invocable<impl, Args...> //
+                (::std::invocable<Projector, Args> && ...) &&
+                ::std::invocable<Fn, ::std::invoke_result_t<Projector, Args>...> //
             )
-        constexpr decltype(auto) operator()(Args&&... args) const //
-        {
-            return impl{}(::std::forward<Args>(args)...);
-        }
+        constexpr decltype(auto) operator()(Fn&& fn, Projector projector, Args&&... args) const
+            noexcept(concepts::nothrow_invocable<Fn, ::std::invoke_result_t<Projector, Args>...> //
+            )
 
-        template<typename... Args>
-            requires functional::cpo_invocable<projected_invoke_fn, Args...>
-        constexpr decltype(auto) operator()(Args&&... args) const
         {
-            return functional::cpo(*this, ::std::forward<Args>(args)...);
+            return ::std::invoke(
+                ::std::forward<Fn>(fn), //
+                ::std::invoke(projector, ::std::forward<Args>(args))... //
+            );
         }
-
     } projected_invoke{};
 
     template<typename... Args>
