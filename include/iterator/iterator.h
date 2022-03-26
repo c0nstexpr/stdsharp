86587307808129d5ad6dd1da0f49458877af7b8b
Blurring Shadow <976999484@qq.com>
2022-03-26 17:34:47 +0800

    feat(indirect): add indirect class indirectly read value or write value

--- include/iterator/iterator.h
+++ include/iterator/iterator.h
@@ -4,10 +4,50 @@
 
 #pragma once
 #include <iterator>
+#include "type_traits/core_traits.h"
 #include "concepts/concepts.h"
 
 namespace stdsharp::iterator
 {
+    template<typename T>
+    concept referenceable = requires
+    {
+        typename ::std::add_lvalue_reference<T>;
+    };
+
+    template<typename T>
+    concept dereferenceable = requires(T& t)
+    { // clang-format off
+        { *t } -> referenceable; // clang-format on
+    };
+
+    namespace details
+    {
+        template<typename T>
+        struct iter_const_reference;
+
+        template<typename T>
+            requires requires { typename ::std::iter_value_t<T>; }
+        struct iter_const_reference<T>
+        {
+            using type = typename type_traits::add_const_lvalue_ref_t<::std::iter_value_t<T>>;
+        };
+
+        template<typename T>
+            requires requires
+            {
+                typename ::std::iter_value_t<T>;
+                typename T::const_reference;
+            }
+        struct iter_const_reference<T>
+        {
+            using type = typename T::const_reference;
+        };
+    }
+
+    template<typename T>
+    using iter_const_reference_t = typename details::iter_const_reference<T>;
+
     template<typename I>
     concept weakly_decrementable = ::std::movable<I> && requires(I i)
     {
