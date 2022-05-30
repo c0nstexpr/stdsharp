f9051bbdd940516406186d6c49e019beae7fa1e6
残象之形 <976999484@qq.com>
2022-05-30 14:28:34 +0800

    test(utility-test): add forward_like test

--- tests/src/utility/utility_test.cpp
+++ tests/src/utility/utility_test.cpp
@@ -3,5 +3,54 @@
 
 namespace stdsharp::test
 {
-    boost::ut::suite& utility_test() noexcept { return boost::ut::get_empty_suite(); }
+    namespace
+    {
+        template<typename T, typename U, typename Expect>
+        struct forward_like_param
+        {
+        };
+
+        template<typename T, typename U>
+        using copy_const_t = ::std::conditional_t<concepts::const_<T>, const U, U>;
+    }
+
+
+    boost::ut::suite& utility_test() noexcept
+    {
+        static boost::ut::suite suite = []
+        {
+            using namespace std;
+            using namespace boost::ut;
+            using namespace bdd;
+
+            feature("forward_like") =
+                []<typename T, typename U, typename Expect>(const forward_like_param<T, U, Expect>)
+            {
+                using res_t = forward_like_t<T, U>;
+
+                print( //
+                    fmt::format(
+                        "owner type: {} \tmember type: {}",
+                        reflection::type_name<T>(),
+                        reflection::type_name<U>() // clang-format off
+                    ) // clang-format on
+                );
+
+                static_expect<same_as<res_t, Expect>>() << fmt::format(
+                    "expect type: {} \tactual type: {}",
+                    reflection::type_name<Expect>(),
+                    reflection::type_name<res_t>() //
+                );
+            } |
+                tuple{
+                    forward_like_param<int&, int&, int&>{},
+                    forward_like_param<int&, int&&, int&>{},
+                    forward_like_param<int&&, const int&, const int&&>{},
+                    forward_like_param<int&, const int&&, const int&>{},
+                    forward_like_param<const int&, int&, const int&>{},
+                    forward_like_param<const int&&, int&, const int&&>{},
+                };
+        };
+        return suite;
+    }
 }
