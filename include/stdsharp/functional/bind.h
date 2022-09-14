4e3692b01cbb4834f46c65e443a07b0e7cc659dd
残象之形 <976999484@qq.com>
2022-09-14 17:37:58 +0800

    refactor(functional-bind): remove lazy invoke

--- include/stdsharp/functional/bind.h
+++ include/stdsharp/functional/bind.h
@@ -26,28 +26,31 @@ namespace stdsharp::functional
         {
         }
 
-#define BS_OPERATOR(const_, ref)                                                            \
-                                                                                            \
-private:                                                                                    \
-    template<::std::size_t... N, typename... Args>                                          \
-    constexpr decltype(auto) operator()(const ::std::index_sequence<N...>, Args&&... args)  \
-        const_ ref noexcept(                                                                \
-            concepts::nothrow_invocable<const_ Func ref, const_ T ref..., Args...>)         \
-    {                                                                                       \
-        return ::std::invoke(                                                               \
-            static_cast<get_t<N, const_ base ref>>(                                         \
-                ::std::get<N>(static_cast<const_ base ref>(*this)))...,                     \
-            ::std::forward<Args>(args)...);                                                 \
-    }                                                                                       \
-                                                                                            \
-public:                                                                                     \
-    template<typename... Args>                                                              \
-        requires ::std::invocable<const_ Func ref, const_ T ref..., Args...>                \
-    constexpr decltype(auto) operator()(Args&&... args) const_ ref noexcept(                \
-        concepts::nothrow_invocable<const_ Func ref, const_ T ref..., Args...>)             \
-    {                                                                                       \
-        return static_cast<const_ bind_t ref>(                                              \
-            *this)(::std::index_sequence_for<Func, T...>{}, ::std::forward<Args>(args)...); \
+#define BS_OPERATOR(const_, ref)                                                                  \
+                                                                                                  \
+private:                                                                                          \
+    template<::std::size_t... N, typename... Args>                                                \
+    constexpr decltype(auto) operator()(                                                          \
+        const ::std::index_sequence<N...>,                                                        \
+        Args&&... args                                                                            \
+    ) const_ ref noexcept(concepts::nothrow_invocable<const_ Func ref, const_ T ref..., Args...>) \
+    {                                                                                             \
+        return ::std::invoke(                                                                     \
+            static_cast<get_t<N, const_ base ref>>(                                               \
+                ::std::get<N>(static_cast<const_ base ref>(*this))                                \
+            )...,                                                                                 \
+            ::std::forward<Args>(args)...                                                         \
+        );                                                                                        \
+    }                                                                                             \
+                                                                                                  \
+public:                                                                                           \
+    template<typename... Args>                                                                    \
+        requires ::std::invocable<const_ Func ref, const_ T ref..., Args...>                      \
+    constexpr decltype(auto) operator()(Args&&... args                                            \
+    ) const_ ref noexcept(concepts::nothrow_invocable<const_ Func ref, const_ T ref..., Args...>) \
+    {                                                                                             \
+        return static_cast<const_ bind_t ref>(*this                                               \
+        )(::std::index_sequence_for<Func, T...>{}, ::std::forward<Args>(args)...);                \
     }
 
         BS_OPERATOR(, &)
@@ -80,24 +83,4 @@ public:                                                                         
 
     template<typename... Args>
     concept nothrow_bindable = concepts::nothrow_invocable<bind_fn, Args...>;
-
-    template<typename... Args>
-    using bind_type = ::std::invoke_result_t<bind_fn, Args...>;
-
-    inline constexpr struct lazy_invoke_fn
-    {
-        template<typename... Args, ::std::invocable<Args...> Func>
-            requires bindable<Func, Args...>
-        [[nodiscard]] constexpr auto operator()(Func&& func, Args&&... args) const
-            noexcept(nothrow_bindable<Func, Args...>)
-        {
-            return bind(::std::forward<Func>(func), ::std::forward<Args>(args)...);
-        }
-    } lazy_invoke{};
-
-    template<typename... Args>
-    concept lazy_invocable = ::std::invocable<lazy_invoke_fn, Args...>;
-
-    template<typename... Args>
-    concept nothrow_lazy_invocable = concepts::nothrow_invocable<lazy_invoke_fn, Args...>;
 }
