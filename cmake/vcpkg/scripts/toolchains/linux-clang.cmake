3cfc65c53d165ab473fa7b516b673ab94a1b9b56
残象之形 <976999484@qq.com>
2022-10-12 11:06:35 +0800

    ci(gh-action): optimize workflow

--- cmake/vcpkg/scripts/toolchains/linux-clang.cmake
+++ cmake/vcpkg/scripts/toolchains/linux-clang.cmake
@@ -1,6 +1,11 @@
 set($ENV{CC} "clang")
 set($ENV{CXX} "clang++")
 
+set(CMAKE_CXX_COMPILER "clang++")
+set(CMAKE_C_COMPILER "clang")
+set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS} -stdlib=libc++")
+set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS}")
+
 set(VCPKG_LINKER_FLAGS "${VCPKG_LINKER_FLAGS} --ld-path=ld.lld")
 
 include(${CMAKE_CURRENT_LIST_DIR}/linux.cmake)
