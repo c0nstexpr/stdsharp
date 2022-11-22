4b9800ee58b9e93468b675d0d206e90a7661a732
残象之形 <976999484@qq.com>
2022-11-22 16:38:46 +0800

    refactor(type-sequence): adl-proof

--- include/stdsharp/type_traits/type_sequence.h
+++ include/stdsharp/type_traits/type_sequence.h
@@ -11,7 +11,7 @@ namespace stdsharp
 
     namespace details
     {
-        struct type_seq
+        struct type_seq_conversion
         {
             template<auto... Values>
             static regular_type_sequence<::meta::_t<decltype(Values)>...>
@@ -32,94 +32,103 @@ namespace stdsharp
             template<typename... T>
             using invoke = type_sequence<T...>;
         };
+
+        template<typename... Types>
+        struct type_sequence
+        {
+            struct type :
+                private value_sequence<type_constant<Types>{}...>,
+                private details::type_seq_conversion
+            {
+            private:
+                using base = value_sequence<type_constant<Types>{}...>;
+                using type_seq_conversion::from_value_seq_t;
+                using type_seq_conversion::to_value_seq_t;
+
+            public:
+                using base::size;
+                using base::for_each;
+                using base::for_each_n;
+                using base::find_if;
+                using base::find_if_not;
+                using base::find;
+                using base::count_if;
+                using base::count_if_not;
+                using base::count;
+                using base::all_of;
+                using base::any_of;
+                using base::none_of;
+                using base::contains;
+                using base::adjacent_find;
+                using base::at_v;
+
+                template<template<typename...> typename T>
+                using apply_t = T<Types...>;
+
+                template<::std::size_t I>
+                using get_t = typename decltype(get<I>(base{}))::type;
+
+                template<::std::size_t... I>
+                using at_t = regular_type_sequence<get_t<I>...>;
+
+                template<::std::size_t Size>
+                using back_t = from_value_seq_t<typename base::template back_t<Size>>;
+
+                template<::std::size_t Size>
+                using front_t = from_value_seq_t<typename base::template front_t<Size>>;
+
+                template<typename... Others>
+                using append_t = regular_type_sequence<Types..., Others...>;
+
+                template<typename T = void>
+                using invoke_fn = typename base::template invoke_fn<T>;
+
+                template<typename T = empty_t>
+                static constexpr invoke_fn<T> invoke{};
+
+                template<typename... Others>
+                using append_front_t = regular_type_sequence<Others..., Types...>;
+
+                template<::std::size_t Index, typename... Other>
+                using insert_t = from_value_seq_t< //
+                    typename base::template insert_t<
+                        Index,
+                        static_const_v<type_constant<Other>>... // clang-format off
+                    >
+                >; // clang-format on
+
+                template<::std::size_t... Index>
+                using remove_at_t = from_value_seq_t<typename base::template remove_at_t<Index...>>;
+
+                template<::std::size_t Index, typename Other>
+                using replace_t = from_value_seq_t<
+                    typename base::template replace_t<Index, type_constant<Other>{}>>;
+
+                template<::std::size_t From, ::std::size_t Size>
+                using select_range_t =
+                    from_value_seq_t<typename base::template select_range_t<From, Size>>;
+            };
+        };
     }
 
     template<typename T>
     using as_type_sequence_t = ::meta::apply<details::as_type_sequence, T>;
 
     template<typename... Types>
-    struct type_sequence :
-        private value_sequence<type_constant<Types>{}...>,
-        private details::type_seq
+    struct type_sequence : details::type_sequence<Types...>::type
     {
-    private:
-        using base = value_sequence<type_constant<Types>{}...>;
-        using type_seq::from_value_seq_t;
-        using type_seq::to_value_seq_t;
-
-    public:
-        using base::size;
-        using base::for_each;
-        using base::for_each_n;
-        using base::find_if;
-        using base::find_if_not;
-        using base::find;
-        using base::count_if;
-        using base::count_if_not;
-        using base::count;
-        using base::all_of;
-        using base::any_of;
-        using base::none_of;
-        using base::contains;
-        using base::adjacent_find;
-        using base::at_v;
-
-        template<template<typename...> typename T>
-        using apply_t = T<Types...>;
-
-        template<::std::size_t I>
-        using get_t = typename decltype(get<I>(base{}))::type;
-
-        template<::std::size_t... I>
-        using at_t = regular_type_sequence<get_t<I>...>;
-
-        template<::std::size_t Size>
-        using back_t = from_value_seq_t<typename base::template back_t<Size>>;
-
-        template<::std::size_t Size>
-        using front_t = from_value_seq_t<typename base::template front_t<Size>>;
-
-        template<typename... Others>
-        using append_t = regular_type_sequence<Types..., Others...>;
-
-        template<typename T = void>
-        using invoke_fn = typename base::template invoke_fn<T>;
-
-        template<typename T = empty_t>
-        static constexpr invoke_fn<T> invoke{};
-
-        template<typename... Others>
-        using append_front_t = regular_type_sequence<Others..., Types...>;
-
-        template<::std::size_t Index, typename... Other>
-        using insert_t = from_value_seq_t< //
-            typename base::template insert_t<
-                Index,
-                static_const_v<type_constant<Other>>... // clang-format off
-            >
-        >; // clang-format on
-
-        template<::std::size_t... Index>
-        using remove_at_t = from_value_seq_t<typename base::template remove_at_t<Index...>>;
-
-        template<::std::size_t Index, typename Other>
-        using replace_t =
-            from_value_seq_t<typename base::template replace_t<Index, type_constant<Other>{}>>;
-
-        template<::std::size_t From, ::std::size_t Size>
-        using select_range_t = from_value_seq_t<typename base::template select_range_t<From, Size>>;
     };
 
     template<typename... T>
     using reverse_type_sequence_t = as_type_sequence_t< //
-        details::type_seq::from_value_seq_t<
+        details::type_seq_conversion::from_value_seq_t<
             reverse_value_sequence_t<type_constant<T>{}...> // clang-format off
         >
     >; // clang-format on
 
     template<typename... T>
     using unique_type_sequence_t = as_type_sequence_t< //
-        details::type_seq::from_value_seq_t<
+        details::type_seq_conversion::from_value_seq_t<
             unique_value_sequence_t<type_constant<T>{}...> // clang-format off
         >
     >; // clang-format on
