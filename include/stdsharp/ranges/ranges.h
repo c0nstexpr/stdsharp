f159c4b74b119559b8e4113739c8e5a1d02595a0
残象之形 <976999484@qq.com>
2023-01-09 17:26:24 +0800

    feat(ranges-views-forwarding): add forwarding views

--- include/stdsharp/ranges/ranges.h
+++ include/stdsharp/ranges/ranges.h
@@ -8,7 +8,9 @@
 #include <range/v3/view.hpp>
 #include <fmt/ranges.h>
 
-#include "../type_traits/core_traits.h"
+#include <functional>
+
+#include "../utility/utility.h"
 
 namespace stdsharp
 {
@@ -20,4 +22,16 @@ namespace stdsharp
 
     template<typename T>
     using range_const_reference_t = add_const_lvalue_ref_t<::std::ranges::range_value_t<T>>;
+
+    template<typename T>
+    using forwarding_views = ::std::ranges::transform_view<T, forward_like_fn<T>>;
+
+    namespace views
+    {
+        inline constexpr nodiscard_invocable forwarding =
+            []<typename T>(T&& t) noexcept(nothrow_constructible_from<forwarding_views<T>, T>)
+        {
+            return forwarding_views<T>{::std::forward<T>(t)}; //
+        };
+    }
 }
