057703b9dd6a7272cb4f87aa01fa704899d588e4
残象之形 <976999484@qq.com>
2023-11-17 15:43:12 +0800

    ci(gh-action): update vs script

--- .github/actions/update-msvc/update-vs.ps1
+++ .github/actions/update-msvc/update-vs.ps1
@@ -1,9 +1,8 @@
-powershell;
 Import-Module "gsudoModule";
 gsudo {
     for($count = 1; $true; $count++)
     {
-        $process = Start-Process -PassThru -Wait -FilePath  "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -ArgumentList "update --quiet --norestart --force --channelId VisualStudio.17.Release --installpath ""C:\Program Files\Microsoft Visual Studio\2022\Enterprise""";
+        $process = Start-Process -PassThru -Wait -NoNewWindow -FilePath  "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -ArgumentList "update --quiet --norestart --force --channelId VisualStudio.17.Release --installpath ""C:\Program Files\Microsoft Visual Studio\2022\Enterprise""";
 
         $exit_code = $process.ExitCode;
 
