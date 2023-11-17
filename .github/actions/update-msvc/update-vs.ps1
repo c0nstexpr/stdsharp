fa21d746ce2d1585fc61ab7640f7fc5a55ddee50
残象之形 <976999484@qq.com>
2023-11-17 16:30:56 +0800

    ci(gh-action): update vs script

--- .github/actions/update-msvc/update-vs.ps1
+++ .github/actions/update-msvc/update-vs.ps1
@@ -1,13 +1,10 @@
-Import-Module "gsudoModule";
-gsudo {
-    for($count = 1; $true; $count++)
-    {
-        $process = Start-Process -PassThru -Wait -NoNewWindow -FilePath  "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -ArgumentList "update --quiet --norestart --force --channelId VisualStudio.17.Release --installpath ""C:\Program Files\Microsoft Visual Studio\2022\Enterprise""";
+for($count = 1; $true; $count++)
+{
+    $process = Start-Process -PassThru -Wait -NoNewWindow -FilePath  "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -ArgumentList "update --quiet --norestart --force --channelId VisualStudio.17.Release --installpath ""C:\Program Files\Microsoft Visual Studio\2022\Enterprise""";
 
-        $exit_code = $process.ExitCode;
+    $exit_code = $process.ExitCode;
 
-        if($exit_code -eq 0) { break }
+    if($exit_code -eq 0) { break }
 
-        echo "exited with $exit_code, tries count: $count. Retrying..."
-    }
+    echo "exited with $exit_code, tries count: $count. Retrying..."
 }
