ec7bff6245fcae3d57cfbc53c122a9a3a0a0b44d
残象之形 <976999484@qq.com>
2023-11-17 15:13:21 +0800

    ci(gh-action): update vs script

---
+++ .github/actions/update-msvc/update-vs.ps1
@@ -0,0 +1,14 @@
+powerhsell;
+Import-Module "gsudoModule;
+gsudo {
+    for($count = 1; $true; $count++)
+    {
+        $process = Start-Process -PassThru -Wait -FilePath  "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" -ArgumentList "update --quiet --norestart --force --channelId VisualStudio.17.Release --installpath ""C:\Program Files\Microsoft Visual Studio\2022\Enterprise""";
+
+        $exit_code = $process.ExitCode;
+
+        if($exit_code -eq 0) { break }
+
+        echo "exited with $exit_code, tries count: $count"
+    }
+}
