970b32dc3260eba0b3974f450e962230eac643d7
Blurring Shadow <976999484@qq.com>
2023-12-13 09:26:04 +0800

    feat(is-iter-in)

--- include/stdsharp/ranges/ranges.h
+++ include/stdsharp/ranges/ranges.h
@@ -74,4 +74,25 @@ namespace stdsharp
         template<typename U>
         inline constexpr auto cast = std::ranges::views::transform(cast_to<U>);
     }
+
+    inline constexpr struct is_iter_in_fn
+    {
+        template<typename I, std::sentinel_for<I> S>
+            requires std::sentinel_for<I, I>
+        constexpr bool operator()(I begin, const S& end, const I& in) const noexcept
+        {
+            if(!std::is_constant_evaluated()) return begin <= in && in < end;
+
+            for(; begin != end; ++begin)
+                if(begin == in) return true;
+            return false;
+        }
+
+        template<std::ranges::range R>
+            requires std::sentinel_for<std::ranges::iterator_t<R>, std::ranges::iterator_t<R>>
+        constexpr bool operator()(R&& r, const const_iterator_t<R>& in) const noexcept
+        {
+            return (*this)(std::ranges::cbegin(r), std::ranges::cend(r), in);
+        }
+    } is_iter_in{};
 }
